624fad686b9b313648311260c8e651241dae0d22
Lalon Sheikh <you@example.com>
2024-08-12 23:38:30 +0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
+// const express = require('express');
+// const cors = require('cors');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// require('dotenv').config()
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// // middleware
+// app.use(cors());
+// app.use(express.json());
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+//     const productCollection = client.db('emaJohnDB').collection('products');
+
+// app.get('/products', async (req, res) => {
+//   const result = await productCollection.find().toArray();
+//   res.send(result);
+// })
+
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+// app.get('/', (req, res) => {
+//   res.send('john is busy shopping')
+// })
+
+// app.listen(port, () => {
+//   console.log(`ema john server is running on port: ${port}`);
+// })
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,11 +65,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gbneo1a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,11 +85,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+
     const productCollection = client.db('emaJohnDB').collection('products');
 
-    app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
-        res.send(result);
+    app.get('/products', async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
@@ -46,10 +104,11 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-    res.send('john is busy shopping')
+
+app.get('/', (req, res) => {
+  res.send('emaJohn is running')
 })
 
-app.listen(port, () =>{
-    console.log(`ema john server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`Ema john server is running on ${port}`)
 })
